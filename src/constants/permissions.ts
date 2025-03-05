3d0bbf513777afdcc4492630adb5ebc918c924fa
hanzala211 <hanzalaoc211@gmail.com>
2025-03-05 22:50:16 +0500

    fix

--- src/constants/permissions.ts
+++ src/constants/permissions.ts
@@ -2,6 +2,6 @@ import { APP_ACTIONS } from "@constants";
 
 export const PERMISSIONS: Record<string, string[]> = {
   admin: [APP_ACTIONS.dashboardPage, APP_ACTIONS.documentPage, APP_ACTIONS.invoicePage, APP_ACTIONS.messagesPage, APP_ACTIONS.addTeam, APP_ACTIONS.customerReviewPage, APP_ACTIONS.supplierPage, APP_ACTIONS.payemntPage, APP_ACTIONS.approvalPage, APP_ACTIONS.teamPage],
-  clerk: [APP_ACTIONS.invoicePage, APP_ACTIONS.teamPage, APP_ACTIONS.invoicePage, APP_ACTIONS.supplierPage],
+  clerk: [APP_ACTIONS.invoicePage, APP_ACTIONS.teamPage, APP_ACTIONS.invoicePage, APP_ACTIONS.supplierPage, APP_ACTIONS.messagesPage],
   approver: [APP_ACTIONS.messagesPage, APP_ACTIONS.teamPage, APP_ACTIONS.approvalPage]
 }
